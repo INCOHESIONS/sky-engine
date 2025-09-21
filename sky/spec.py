9d490d444dc586a1d2f1850618c3db99243fae69
INCOHESIONS <incohesions@gmail.com>
2025-09-21 18:03:22 -0300

    docs

--- sky/spec.py
+++ sky/spec.py
@@ -11,7 +11,7 @@ __all__ = ["AppSpec", "WindowSpec"]
 @final
 @dataclass
 class WindowSpec:
-    """Defines information the window needs to have before mainloop.If position is None, the window will be centered on the screen."""
+    """Defines information the window needs to have before mainloop. If `position` is None, the window will be centered on the screen."""
 
     _: KW_ONLY
     title: str = "Sky Engine"
@@ -23,22 +23,26 @@ class WindowSpec:
     backend: Literal["software", "opengl", "vulkan"] = "software"
 
     def is_software(self) -> bool:
+        """Whether the window is running on a software backend."""
+
         return self.backend == "software"
 
     def is_hardware(self) -> bool:
+        """Whether the window is running on a hardware backend (OpenGL or Vulkan)."""
+
         return not self.is_software()
 
 
 @final
 @dataclass
 class AppSpec:
-    """Defines information the app needs to have before mainloop. If window_spec is None, a window will not be created."""
+    """Defines information the app needs to have before mainloop. If `window_spec` is None, a window will not be created."""
 
     _: KW_ONLY
     window_spec: WindowSpec | None = field(default_factory=WindowSpec)
 
     @classmethod
     def headless(cls) -> AppSpec:
-        """Simply creates an AppSpec with window_spec set to None."""
+        """Simply creates an `AppSpec` with `window_spec` set to None."""
 
         return cls(window_spec=None)
